bcd560e639beea1565ad35b9f7cf8beb0057e8d8
Tony Baldwin <tonyb@myownsite.me>
2015-01-20 02:23:13 +0100

    replaced friendica plugin with redmatrix plugin updated install.sh and readme added a changelog

--- install.sh
+++ install.sh
@@ -1,35 +1,46 @@
 #!/bin/bash
 
 # installer for mylog.sh
-# by tony baldwin - http://tonyb.us/dream
+# by tony baldwin - http://tonybaldwin.me
 
-if [ != "$HOME/bin/" ]; then
+echo "installing mylog.sh ... "
+
+if [ ! i-d  "$HOME/bin/" ]; then
 	mkdir $HOME/bin/
 	$PATH=$PATH:/$HOME/bin/
 	export PATH
 fi
+if [ ! -d "$HOME/.mylog" ]; then
+	mkdir $HOME/.mylog
+fi
 
-
-echo "installing mylog.sh ... "
 cp mylog $HOME/bin/mylog
 chmod +x $HOME/bin/mylog
 
 echo "Creating config files ... "
 read -p "What is your prefered editor? (default is vim) " ed
-echo "The following are only needed if you use and will post to friendica."
-read -p "Friendica username: " $fuser
-read -p "Friendica password: " $fpass
-read -p "Friendica site (include https://yourfriendicasite.tld): " $fsite
 if [[ $ed != "" ]]; then
 	editor=$ed
 else
 	editor="/usr/bin/vim"
 fi
-
-mkdir $HOME/.mylog
 echo "# mylog.sh config file" > $HOME/.mylog.conf
-echo "$HOME/.mylog/" >> $HOME/.mylog.conf
+echo "mylogdir=$HOME/.mylog/" >> $HOME/.mylog.conf
 echo "editor=$editor" >> $HOME/.mylog.conf
+
+read -p "Will you use the redmatrix posting feature? (y/n)" rplug
+if [ $rplug = y ]; then 
+read -p "RedMatrix username: " $ruser
+read -p "RedMatrix password: " $rpass
+read -p "RedMatrix site (include https://yourredmatrixsite.tld): " $rurl
+echo "rplug=y" >> $HOME/.mylog.conf
+echo "ruser=$ruser" >> $HOME/.mylog.conf
+echo "rpass=$rpass" >> $HOME/.mylog.conf
+echo "rurl=$rurl" >> $HOME/.mylog.conf
+else
+	echo "rplug=nope" >> $HOME/.runlog.conf
+fi
+
 
 echo "Installation complete."
 
